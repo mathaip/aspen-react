1bd90ecf7865d38a91786ce9ce4e32d75d64493c
mathaip <mputhiakunnel@gmail.com>
2021-10-01 19:08:01 +0530

    added to gitignore

--- aspendreact/src/Pages/MarketPlace.js
+++ aspendreact/src/Pages/MarketPlace.js
@@ -10,14 +10,13 @@ import taiping from '../images/taiping-rebellion.png'
 import cuban from '../images/cuban-revolution-pack.png'
 
 function MarketPlace() {
-    const products = [
-        {
+    const products = [{
             name: 'American Revolutuion',
             img: product,
             description: '59 Packs',
-            price: 'USD $150.00 (Eth)',
+            price: 'USD $80.00 (BNB)',
             releaseDate: '4th of July',
-            path:'/AmericanRevolution'
+            path: '/AmericanRevolution'
         },
         {
             name: 'French Revolutuion',
@@ -68,35 +67,49 @@ function MarketPlace() {
         alignItems: "start",
         paddingTop: "60px"
     };
-    return (
-        <section className="showcase" style={alignTop}>
-            <SideNav />
-            <div className="main">
-                <NavBar />
-                <h1 className="text-center">Upcoming Collection Drops</h1>
-                <div className="row mt-4">
-                    {
-                        products.map(product => (
-                            <div className="card__product--container col-md-4 col-6">
-                                <NavLink to={product.path} className="card__product--link">
-                                    <div className="card__product col-12">
-                                        <div className="card__product--img-container">
-                                            <img src={product.img} alt="card product diagram" className="card__product--img" />
-                                        </div>
-                                        <h4 className="card__product--heading col-12 text-center">{product.name || ''} </h4>
-                                        <p className="card__product--text col-12 text-center">{product.description || ''}</p>
-                                        <p className="card__product--text col-12 text-center">{product.price || ''}</p>
+    return ( <
+        section className = "showcase"
+        style = { alignTop } >
+        <
+        SideNav / >
+        <
+        div className = "main" >
+        <
+        NavBar / >
+        <
+        h1 className = "text-center" > Upcoming Collection Drops < /h1> <
+        div className = "row mt-4" > {
+            products.map(product => ( <
+                div className = "card__product--container col-md-4 col-6" >
+                <
+                NavLink to = { product.path }
+                className = "card__product--link" >
+                <
+                div className = "card__product col-12" >
+                <
+                div className = "card__product--img-container" >
+                <
+                img src = { product.img }
+                alt = "card product diagram"
+                className = "card__product--img" / >
+                <
+                /div> <
+                h4 className = "card__product--heading col-12 text-center" > { product.name || '' } < /h4> <
+                p className = "card__product--text col-12 text-center" > { product.description || '' } < /p> <
+                p className = "card__product--text col-12 text-center" > { product.price || '' } < /p>
 
-                                        <p className="card__product--price col-12 text-center">{product.releaseDate || ''}</p>
-                                    </div>
-                                </NavLink>
-                            </div>
-                        ))
-                    }
+                <
+                p className = "card__product--price col-12 text-center" > { product.releaseDate || '' } < /p> <
+                /div> <
+                /NavLink> <
+                /div>
+            ))
+        }
 
-                </div>
-            </div>
-        </section>
+        <
+        /div> <
+        /div> <
+        /section>
     )
 }
 
--- aspendreact/src/currentproviderweb3.js
+++ aspendreact/src/currentproviderweb3.js
@@ -4,19 +4,19 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 let currentproviderweb3;
 
-const mnemonicPhrase = 'breeze kitchen hedgehog sketch ball cheap lawsuit human impose group asthma vocal'
+const mnemonicPhrase = process.env.MNEMONIC
 let provider = new HDWalletProvider({
-  mnemonic: {
-    phrase: mnemonicPhrase
-  },
-  providerOrUrl: "https://mainnet.infura.io/v3/47257472d21d45d39769bf5c1fed44b5"
+    mnemonic: {
+        phrase: mnemonicPhrase
+    },
+    providerOrUrl: "https://mainnet.infura.io/v3/47257472d21d45d39769bf5c1fed44b5"
 });
 
 
 if (!window.web3) {
     currentproviderweb3 = new Web3(provider)
-} else{
-     currentproviderweb3 = new Web3(window.web3.currentProvider);
+} else {
+    currentproviderweb3 = new Web3(window.web3.currentProvider);
 
 }
 
